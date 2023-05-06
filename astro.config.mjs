fe77ce05fb1a62b73d35d01e80fee4dabb7b3705
kimagin <info@kimagination.net>
2023-05-06 12:33:27 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import alpinejs from "@astrojs/alpinejs";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [alpinejs(), tailwind()]
+});
